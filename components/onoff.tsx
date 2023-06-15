ae9ab8582d49896d797f3698491d2e41a09b04d1
BernardoSantosLop <71346080+BernardoSantosLop@users.noreply.github.com>
2023-06-14 23:16:48 -0600

    Add files via upload

---
+++ components/onoff.tsx
@@ -0,0 +1,17 @@
+import Switch from '../components/switch'
+import Link from 'next/link';
+
+const onoff = () => {
+    return(
+        <>
+        <div className='bg-white text-black p-10'>
+            <div>
+                <h1>LCCONTROLER</h1>
+                <p>Controlador de Proceso IOT</p>
+                <p>Sistema fraccionario para destilar CBD</p>
+            </div>
+        </div>
+        </>
+    );
+};
+export default onoff;
